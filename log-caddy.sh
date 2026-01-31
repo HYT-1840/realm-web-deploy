15a473f2c76c75acda55634ef223c3ee3ba1aa5d
HYT1840 <hyt1840@gmail.com>
2026-01-31 21:46:29 +0800

    Create log-caddy.sh

---
+++ log-caddy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+red() { echo -e "\033[31m$1\033[0m"; }
+info() { echo -e "\033[36m$1\033[0m"; }
+
+# 检查root
+[[ $EUID -ne 0 ]] && { red "❌ 请使用root权限执行"; exit 1; }
+
+# 实时查看日志
+info "📜 正在查看Caddy实时日志（按Ctrl+C退出）..."
+journalctl -u caddy -f --no-pager
